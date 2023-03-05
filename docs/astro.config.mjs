6d88172376f43f0af61837e0712336196b3491db
SorcererOfKittens <126662398+cjkitten@users.noreply.github.com>
2023-03-05 11:36:00 +0800

    Update astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -10,5 +10,5 @@ export default defineConfig({
 		// Enable React for the Algolia search component.
 		react(),
 	],
-	site: `https://cjkitten.github.io`,
+	site: `https://cjkitten.github.io/docs`,
 });
