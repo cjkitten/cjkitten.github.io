225657b86acd8733d046ba03849365b71495fb48
SorcererOfKittens <126662398+cjkitten@users.noreply.github.com>
2023-03-05 12:07:56 +0800

    Update consts.ts

--- docs/src/consts.ts
+++ docs/src/consts.ts
@@ -1,15 +1,13 @@
 export const SITE = {
-	title: 'Documentation',
-	description: 'Your website description.',
+	title: 'CJKitten Documentation',
+	description: 'CJKitten Documentation',
 	defaultLanguage: 'en-us',
 } as const;
 
 export const OPEN_GRAPH = {
 	image: {
-		src: 'https://github.com/withastro/astro/blob/main/assets/social/banner-minimal.png?raw=true',
-		alt:
-			'astro logo on a starry expanse of space,' +
-			' with a purple saturn-like planet floating in the right foreground',
+		src: 'https://cjkitten.github.io/docs/public/CJKitten%20Documentation.png',
+		alt: 'A Life with a beautiful nature by your side.'	
 	},
 	twitter: 'astrodotbuild',
 };
@@ -19,16 +17,7 @@ export const KNOWN_LANGUAGES = {
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
-
-export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
-
-// See "Algolia" section of the README for more information.
-export const ALGOLIA = {
-	indexName: 'XXXXXXXXXX',
-	appId: 'XXXXXXXXXX',
-	apiKey: 'XXXXXXXXXX',
-};
+export const GITHUB_EDIT_URL = `https://github.com/cjkitten/cjkitten.github.io/tree/main/docs`;
 
 export type Sidebar = Record<
 	(typeof KNOWN_LANGUAGE_CODES)[number],
@@ -36,10 +25,10 @@ export type Sidebar = Record<
 >;
 export const SIDEBAR: Sidebar = {
 	en: {
-		'Section Header': [
-			{ text: 'Introduction', link: 'en/introduction' },
-			{ text: 'Page 2', link: 'en/page-2' },
-			{ text: 'Page 3', link: 'en/page-3' },
+		'Projects': [
+			{ text: 'Introduction', link: 'introduction' },
+			{ text: 'Key30', link: 'key30' },
+			{ text: 'page3', link: 'en/page-3' },
 		],
 		'Another Section': [{ text: 'Page 4', link: 'en/page-4' }],
 	},
